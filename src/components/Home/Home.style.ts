df9ece7844bc94e029e237fa3c6ed91727089a8f
ykwimk <ykwimk@gmail.com>
2022-02-08 13:07:30 +0900

    Feat: height 값 추가

--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -1,3 +1,5 @@
 import styled from '@emotion/styled';
 
-export const HomeWrapper = styled('div')``;
+export const HomeWrapper = styled('div')`
+  height: 100%;
+`;
--- src/components/List/List.style.ts
+++ src/components/List/List.style.ts
@@ -1,44 +1,5 @@
 import styled from '@emotion/styled';
 
 export const ListWrapper = styled('div')`
-  li {
-    margin-bottom: 10px;
-    padding: 15px;
-    cursor: pointer;
-    &:hover {
-      background: #f6f6f6;
-    }
-  }
-  .list-item {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .photo {
-    display: inline-block;
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-    overflow: hidden;
-    margin-right: 15px;
-  }
-  .content {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .information {
-      .full-name {
-        font-weight: bold;
-        margin-bottom: 4px;
-      }
-      .cell,
-      .email {
-        font-size: 13px;
-      }
-    }
-    .icon {
-      font-size: 21px;
-    }
-  }
+  height: 100%;
 `;
